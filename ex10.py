9d27f62e98834a1679bef68bfd73867a76aa19b2
James Liao <james.liao@outlook.com>
2022-02-23 22:40:36 -0800

    ex10

--- ex10.py
+++ ex10.py
@@ -13,3 +13,12 @@ print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+height = "I am 6'2\" tall."  # escape double-quote inside string
+height2 = 'I am 6\'2" tall.'  # escape single-quote inside string
+height3 = """
+I am 6'2" tall
+I am 6'2" tall
+"""
+
+print(f"{height} \n{height2} {height3}")
