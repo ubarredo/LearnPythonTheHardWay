d0a5a855d00aff952351d091942b8e12e2f05907
James Liao <james.liao@outlook.com>
2022-02-23 21:36:27 -0800

    ex04

--- ex04.py
+++ ex04.py
@@ -1,10 +1,25 @@
+# number of cars
 cars = 100
+
+# number of people can fit into a car
 space_in_a_car = 4.0
+
+# number of drivers
 drivers = 30
+
+# number of passengers
 passengers = 90
+
+# number of cars without drivers
 cars_not_driven = cars - drivers
+
+# number of cars with drivers
 cars_driven = drivers
+
+# number of people can fit into all cars
 carpool_capacity = cars_driven * space_in_a_car
+
+# average number of passengers in each car
 average_passengers_per_car = passengers / cars_driven
 
 print("There are", cars, "cars available.")
