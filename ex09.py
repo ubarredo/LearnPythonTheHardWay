66c301bd853eb4ea116e496d9d7a67f00877e00a
James Liao <james.liao@outlook.com>
2022-02-23 22:33:30 -0800

    ex09

--- ex09.py
+++ ex09.py
@@ -1,7 +1,7 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print("Here are the days:", days)
 print("Here are the months: ", months)
