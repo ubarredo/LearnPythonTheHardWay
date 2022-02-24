2c9515c51f36329d6e0e5bb46acf7091b482955f
James Liao <james.liao@outlook.com>
2022-02-23 21:42:41 -0800

    ex05

--- ex05.py
+++ ex05.py
@@ -1,18 +1,23 @@
-my_name = 'Zed A. Shaw'
-my_age = 35  # not a lie
-my_height = 74  # inches
-my_weight = 180  # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35  # not a lie
+height = 74  # inches
+height_cm = round(height / 2.54) # centimeters
+weight = 180  # lbs
+weight_kg = round(height / 0.45359237) # kilograms
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+print(f"He's {weight_kg} kilograms heavy.")
+print(f"He's {height_cm} centimeters tall.")
